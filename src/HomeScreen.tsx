ca1c1f90969ab1b33cd1376178dcb34b00bf81be
ShabinMuhammedKK <shabinmuhammedkk@gmail.com>
2025-03-04 12:48:47 +0530

    home screen added

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,24 +1,56 @@
-import {StyleSheet, Text, StatusBar, ImageBackground} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  StatusBar,
+  ImageBackground,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
 
-const bgImage = require('../assets/images/video_bg.png');
+const bgImage = require('../assets/images/home_bg.jpg');
 
-const HomeScreen: React.FC = () => {
+const screenDiamention = Dimensions.get('screen');
+
+const HomeScreen = () => {
+  const [dimensions, setDimensions] = useState(screenDiamention);
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
+      <StatusBar
+        barStyle={'light-content'}
+        translucent={true}
+        backgroundColor={'transparent'}
+      />
       <ImageBackground
         source={bgImage}
         resizeMode="cover"
         style={styles.bgImage}>
-        <StatusBar
-          barStyle={'light-content'}
-          translucent={true}
-          backgroundColor={'transparent'}
-        />
-        <Text>HomeScreen</Text>
+        <View
+          style={[
+            styles.bottomContainer,
+            {width: dimensions.width, height: (dimensions.height * 4) / 10},
+          ]}>
+          <View
+            style={[
+              styles.titleContainer,
+              {padding: (dimensions.width * 10) / 100},
+            ]}>
+            <Text style={styles.title}>Player</Text>
+            <Text style={styles.title}>
+              That <Text style={{color: '#ea2379'}}>Plays</Text>
+            </Text>
+            <Text style={styles.title}>All</Text>
+            <Text style={{color: 'gray'}}>
+              Ultimate solution to your media capabilites
+            </Text>
+            <TouchableOpacity style={styles.btn}>
+              <Text>wg</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </ImageBackground>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -27,13 +59,36 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    // justifyContent: 'center',
-    // alignItems: 'center',
+    position: 'relative',
   },
   bgImage: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
+  },
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    flex: 1,
+  },
+  titleContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  title: {
+    color: 'white',
+    fontSize: 44,
+    fontWeight: '700',
+  },
+  btn: {
+    backgroundColor: '#ea2379',
+    height: 70,
+    marginTop: 20,
+    borderRadius: 50,
+    width: 130,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
