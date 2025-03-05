294462d7f34491047a755d9fdf1a46932a7c224f
ShabinMuhammedKK <shabinmuhammedkk@gmail.com>
2025-03-05 10:12:11 +0530

    add homescreen animations and custom fonts

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -6,15 +6,30 @@ import {
   View,
   Dimensions,
   TouchableOpacity,
+  Animated,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const bgImage = require('../assets/images/home_bg.jpg');
-
 const screenDiamention = Dimensions.get('screen');
 
 const HomeScreen = () => {
   const [dimensions, setDimensions] = useState(screenDiamention);
+
+  const popUpAnim = useRef(new Animated.Value(dimensions.height)).current;
+
+  useEffect(() => {
+    const popUp = setTimeout(() => {
+      Animated.timing(popUpAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }, 500);
+    return () => clearTimeout(popUp);
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar
@@ -26,29 +41,37 @@ const HomeScreen = () => {
         source={bgImage}
         resizeMode="cover"
         style={styles.bgImage}>
-        <View
+        <Animated.View
           style={[
             styles.bottomContainer,
-            {width: dimensions.width, height: (dimensions.height * 4) / 10},
+            {
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              right: 0,
+              width: dimensions.width,
+              height: (dimensions.height * 4) / 10,
+              transform: [{translateY: popUpAnim}],
+            },
           ]}>
           <View
             style={[
               styles.titleContainer,
-              {padding: (dimensions.width * 10) / 100},
+              {paddingHorizontal: (dimensions.width * 10) / 100},
             ]}>
             <Text style={styles.title}>Player</Text>
             <Text style={styles.title}>
               That <Text style={{color: '#ea2379'}}>Plays</Text>
             </Text>
             <Text style={styles.title}>All</Text>
-            <Text style={{color: 'gray'}}>
+            <Text style={{color: 'gray', fontFamily: 'AirbnbCereal-light'}}>
               Ultimate solution to your media capabilites
             </Text>
             <TouchableOpacity style={styles.btn}>
-              <Text>wg</Text>
+              <Icon name="arrow-top-right" size={46} color="#fff" />
             </TouchableOpacity>
           </View>
-        </View>
+        </Animated.View>
       </ImageBackground>
     </View>
   );
@@ -65,29 +88,27 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   bottomContainer: {
-    position: 'absolute',
-    bottom: 0,
     flex: 1,
   },
   titleContainer: {
     flex: 1,
     backgroundColor: 'black',
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
     justifyContent: 'center',
     alignItems: 'flex-start',
   },
   title: {
     color: 'white',
-    fontSize: 44,
-    fontWeight: '700',
+    fontSize: 48,
+    fontFamily: 'AirbnbCereal-Bold',
   },
   btn: {
     backgroundColor: '#ea2379',
-    height: 70,
+    height: 50,
     marginTop: 20,
     borderRadius: 50,
-    width: 130,
+    width: 100,
     justifyContent: 'center',
     alignItems: 'center',
   },
